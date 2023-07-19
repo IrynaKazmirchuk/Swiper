492371d45e2d736ccff22f7267d15ad8873692fe
IrynaKazmirchuk <irakazm@gmail.com>
2023-07-19 17:38:46 +0300

    init swiper

---
+++ script.js
@@ -0,0 +1,17 @@
+new Swiper('.image-slider', {
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        // type: 'bullets',
+        // clickable: true,
+        // dynamicBullets: true,
+        // renderBullet: function (index, className) {
+        //     return '<span class="' + className + '">' + (index + 1) + '</span>';
+        // },
+        type: 'fraction',
+    },
+});
